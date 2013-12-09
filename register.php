f984f304ac71a13746ca984a95361ea718a7e62f
Aaron Holland <aaronholla@gmail.com>
2013-12-09 02:57:24 -0500

    Edited Links
    Changed links from absolute to relative.

--- register.php
+++ register.php
@@ -11,9 +11,9 @@ include('session.php');
 			<div id="background">
 			    <!--<img src="102_0273.jpg" alt="" width="1600px" height="1200px"/>-->
 			</div>
-			<div class="register-logo"><img src="/images/bop-logo-beta-full.png" alt="BoP Logo" width="300"></div>
+			<div class="register-logo"><img src="images/bop-logo-beta-full.png" alt="BoP Logo" width="300"></div>
 			<div id="registerbox">
-			<form name="register" id="registerform" action="/process.php" method="post">
+			<form name="register" id="registerform" action="process.php" method="post">
 				<b>Register</b></br>
 				<i>Please fill out the information below.</i>
 				
@@ -44,7 +44,7 @@ include('session.php');
 				unset($_SESSION['email']);
 			?>
 			<div id="bottombox">
-				<i>Already have an account?</i><a name="reg_link" href="/"/>Login</a>
+				<i>Already have an account?</i><a name="reg_link" href="login.php"/>Login</a>
 			</div>
 			</div>
 	</div>
