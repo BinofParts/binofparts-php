8f867d6654b614c5e912d5e4108b104d073a6d7b
Aaron Holland <aaronholla@gmail.com>
2013-12-16 07:47:50 -0500

    Footer Fix

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
-<div id="footer">
-	This is the footer, all the way down here.
+<div id="footer" class="container">
+	<div class="row">
+		<div class="col-sm-6 col-sm-offset-3 col-md-offset-3 col-lg-offset-3">
+			<hr>
+			This is the footer, all the way down here.
+		</div>
+	</div>
 </div>
 
 <script src="https://code.jquery.com/jquery.js"></script>
