1b420f45e8a7f6e105c598fec1f64f5d07e2e0f7
Aaron Holland <aaronholla@gmail.com>
2013-12-17 03:59:49 -0500

    Updated Custom Team Updater
    Script to update teams directly from FIRST now works properly.
    Currently only updates first 250 and no detailed info from the team
    page. Just a start.

--- tests/frcteamupdate.php
+++ tests/frcteamupdate.php
@@ -2,10 +2,13 @@
 require('simple_html_dom.php');
 
 //add 250 to skip_teams to go to next page.
-$url='https://my.usfirst.org/myarea/index.lasso?page=teamlist&event_type=FRC&sort_teams=number&year=2013&event=flor';
+$url='https://my.usfirst.org/myarea/index.lasso?page=searchresults&programs=FRC&reports=teams&sort_teams=number&results_size=250&omit_searchform=1&season_FRC=2014';
 
 $table = array();
-$teamlist = file_get_html($url);
+$htmlfile = file_get_html($url);
+
+$teamlist = $htmlfile->find("table", 2);
+
 foreach($teamlist->find('tr') as $row) {
     $location = $row->find('td',0)->plaintext;
     $name = $row->find('td',1)->plaintext;
@@ -13,8 +16,10 @@ foreach($teamlist->find('tr') as $row) {
 
 	$table[] = array('number' => $number,'name' => $name, 'location' => $location);
 }
+
+$output = array_slice($table, 3); 
 echo '<pre>';
-print_r($table);
+print_r($output);
 echo '</pre>';
 
 ?>
