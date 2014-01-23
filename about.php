dd2142ce24dc7232a9209e5eb628089409f18907
Aaron Holland <aaronholla@gmail.com>
2014-01-23 01:36:40 -0500

    Updated About Page
    *A fancy image to the jumbotron
    *Info about us and FIRST
    *A jumbotron for quote from first
    *Github buttons at the bottom

--- about.php
+++ about.php
@@ -9,13 +9,38 @@ include_once("session.php");
 <body>
 	<div id="content">
 		<?php include_once("navbar.php"); ?>
-		<div class="jumbotron">
+		<div class="jumbotron missionjumbo">
 			<div class="container">
-				"Providing science and technology accessibility to the students."
+				<div class="mission"><span>"Providing science and technology accessibility to the students."</span></br><span>- Our Mission -</span></div>
 			</div>
 		</div>
-		<div class="container">
+		<div class="container bop-bio">
 			<img class="bop-logo" src="/images/bop-logo-beta-full.png">
+			<div class="col-sm-8 col-sm-offset-2 col-md-offset-2 col-lg-offset-2">
+				<h2>What do we do?</h2>
+				<p>We help FIRST Robotics teams to find the things that they need. More specifically we built a website that will allow FIRST Robotics Competition (FRC) teams to share parts with each other and FIRST.</p>
+			</div>
+			<div class="first-logo col-sm-2 col-xs-offset-4 col-sm-offset-5 col-md-offset-5 col-lg-offset-5">
+				<a href="http://usfirst.org"><img src="/images/FIRSTvert_RGB.jpg" height="120"></a>
+			</div>
+		</div>
+		<div class="container first-bio">
+			<div class="col-sm-8 col-sm-offset-2 col-md-offset-2 col-lg-offset-2">
+				<h2>What is FIRST?</h2>
+				<p>FIRST was founded in 1989 to inspire young people's interest and participation in science and technology. Based in Manchester, NH, the 501 (c) (3) not-for-profit public charity designs accessible, innovative programs that motivate young people to pursue education and career opportunities in science, technology, engineering, and math, while building self-confidence, knowledge, and life skills.</p>
+			</div>
+		</div>
+		<div class="jumbotron">
+			<div class="container">
+				<div class="first-vision col-sm-8 col-sm-offset-2 col-md-offset-2 col-lg-offset-2"><span>"To transform our culture by creating a world where science and technology are celebrated and where young people dream of becoming science and technology leaders."</span></br><span>- Dean Kamen, Founder of FIRST -</span></div>
+			</div>
+		</div>
+		<div class="container github-buttons">
+			<h3>Bin of Parts is Open Source!</h3>
+			<iframe src="http://ghbtns.com/github-btn.html?user=BinofParts&repo=binofparts-php&count=true&type=fork&size=large"
+  allowtransparency="true" frameborder="0" scrolling="0" width="135" height="30"></iframe>
+  			<iframe src="http://ghbtns.com/github-btn.html?user=BinofParts&repo=binofparts-php&count=true&size=large&type=watch"
+  allowtransparency="true" frameborder="0" scrolling="0" width="115" height="30"></iframe>
 		</div>
 		<?php include_once("footer.php"); ?>
 	</div>
