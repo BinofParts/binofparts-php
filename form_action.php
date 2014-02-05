20c920beb1538f7a1e1f33085c70363c633b1f4a
Aaron Holland <aaronholla@gmail.com>
2014-02-05 10:40:02 -0500

    Added temp invite token to register redirect

--- form_action.php
+++ form_action.php
@@ -13,7 +13,8 @@ elseif ($_REQUEST['action'] == "register") {
 	if($database->register($_REQUEST['team'], $_REQUEST['namefirst'], $_REQUEST['namelast'], $_REQUEST['email'], $_REQUEST['pass'], $_REQUEST['type']) == true){
         header('Location: login.php');
     }else{
-		header('Location: register.php');
+        //TODO: Save Invite token to a session variable. Destroy the invite token when registered.
+		header('Location: register.php?invite=test');
     }
 }
 
